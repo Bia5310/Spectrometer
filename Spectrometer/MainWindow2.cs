5a6c029db7e51ce68f62c83e2e196a772c43ca26
Bia5310 <Bia5310@yandex.ru>
2020-03-02 07:22:59 +0300

    Добавил проект камеры

---
+++ Spectrometer/MainWindow2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spectrometer
+{
+    public partial class MainWindow2 : MainWindow
+    {
+        
+    }
+}
